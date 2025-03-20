d1118c58311d0e9d5b390f3864c519b82cc972be
pdahika <124161207+pdahika@users.noreply.github.com>
2025-03-20 18:42:05 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -8,10 +8,10 @@ variable "key_name" {
 
 
 variable "access_key" {
-  default = "AKIAVPEYWK5MQJRX75OK"                # aws access key
+  default = ""                # aws access key
 }
 
 variable "secret_key" {
-  default = "DiEw9l/b6njDqm8rxzT7KM2Xx2zj7EVDUCkhdmHv"         # aws secret key
+  default = ""         # aws secret key
 }
 
